9763409423a2275164cd27afc83e25ff08a572fc
mafien0 <mafien_0@tuta.io>
2026-07-31 10:41:06 +0500

    fix: configure signing jj

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -3,11 +3,12 @@ let
     name = "mafien0";
     email = "mafien_0@tuta.io";
   };
+  signingKey = "~/.ssh/id_ed25519.pub";
 in {
   programs.git = {
     enable = true;
     signing = {
-      key = "~/.ssh/id_ed25519.pub";
+      key = signingKey;
       signByDefault = true;
     };
     settings = {
@@ -24,6 +25,12 @@ in {
     enable = true;
     settings = {
       inherit user;
+
+      signing = {
+        backend = "ssh";
+        key = signingKey;
+        sign-all = true;
+      };
     };
   };
 }
