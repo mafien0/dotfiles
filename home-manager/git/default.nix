f332dacddbe308d64ad4d7fc4eb3848f5357f7ba
mafien0 <mafien_0@tuta.io>
2026-07-31 10:33:15 +0500

    feat: add jujutsu for git version control

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -1,24 +1,29 @@
-{
+let
+  user = {
+    name = "mafien0";
+    email = "mafien_0@tuta.io";
+  };
+in {
   programs.git = {
     enable = true;
-
     signing = {
       key = "~/.ssh/id_ed25519.pub";
       signByDefault = true;
     };
-
     settings = {
-      user.name = "mafien0";
-      user.email = "mafien_0@tuta.io";
-
+      inherit user;
       init.defaultBranch = "main";
-
       tag.gpgSign = true;
-
       gpg = {
         format = "ssh";
         ssh.allowedSignersFile = "~/.ssh/allowed_signers";
       };
+    };
+  };
+  programs.jujutsu = {
+    enable = true;
+    settings = {
+      inherit user;
     };
   };
 }
