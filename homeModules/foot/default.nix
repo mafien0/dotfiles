f312020ade94824673689f5d4267569eb761e5c2
mafien0 <mafien_0@tuta.io>
2026-08-25 20:11:35 +0500

    chore: update flake inputs

--- homeModules/foot/default.nix
+++ homeModules/foot/default.nix
@@ -1,10 +1,18 @@
 {
   stylix.targets.foot.enable = true;
 
-  xdg.terminal-exec.enable = true;
-  xdg.terminal-exec.settings = {
-    default = ["foot.desktop"];
+  xdg = {
+    terminal-exec = {
+      enable = true;
+      settings = {
+        default = ["foot.desktop"];
+      };
+    };
   };
+
+  home.file.".config/xfce4/helpers.rc".text = ''
+    TerminalEmulator=foot-xfce
+  '';
 
   programs.foot = {
     enable = true;
